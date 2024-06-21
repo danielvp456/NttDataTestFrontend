6299512a7781f0eff373062811ddf8282a39fa08
danielvp456 <danielvp456@hotmail.com>
2024-06-21 00:38:15 -0500

    New changes implemented for the home view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { User } from './models/user';
-
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 
 import { GetUserComponent } from './components/get-user/get-user.component';
 import { UserResumeComponent } from './components/user-resume/user-resume.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
+    { path: '', component: HomeComponent },
     { path: 'getUser', component: GetUserComponent },
     { path: 'userResume', component: UserResumeComponent },
 ];
